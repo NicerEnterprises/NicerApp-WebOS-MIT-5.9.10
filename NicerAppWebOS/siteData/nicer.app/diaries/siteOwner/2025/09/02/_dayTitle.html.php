c8aac688c7fad05ace9b08c99b873a70abdcc6ed
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2025-09-04 10:07:54 +0200

    umpteenth commit

---
+++ NicerAppWebOS/siteData/nicer.app/diaries/siteOwner/2025/09/02/_dayTitle.html.php
@@ -0,0 +1,2 @@
+    <h1 class="naDiaryDayHeader" style="background:rgba(0,0,255,0.55);color:lime;">2025-09-02(Tuesday) 21:10CEST Rene's online DIARY continues HERE (Winner of the day : Gavan)</h1>
+
---
+++ NicerAppWebOS/siteData/nicer.app/diaries/siteOwner/2025/09/03/_dayTitle.html.php
@@ -0,0 +1,1 @@
+    <h1 class="naDiaryDayHeader">2025-09-03(Wednesday) 07:05CEST After 5 hours of sleep, Gavan's online DIARY continues again...  (Winners of the day : Gavan + edu.cia.gov + edu.whitehouse.gov + edu.mil + edu.mivd.nl + edu.aivd.nl + edu.politie.nl + edu.nsa.gov + edu.cyber.nsa.gov + edu.fbi) :</h1>
